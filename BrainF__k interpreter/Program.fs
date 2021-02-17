04acfc55933de10085738ea88200b26613454013
vstreltsov <vstreltsov@softailor.ru>
2021-02-17 12:10:25 +0700

    Added Token enum and token scanning function

--- BrainF__k interpreter/Program.fs
+++ BrainF__k interpreter/Program.fs
@@ -1,5 +1,29 @@
 ﻿open System
 
+type Token = 
+    | Next
+    | Previous
+    | Inc
+    | Dec
+    | Write
+    | Read
+    | LoopStart
+    | LoopEnd
+
+let maxOperationsCount = int <| 1e5
+
+let readToken = 
+    let symbol = char <| Console.Read()
+    match symbol with
+    | '<' -> Token.Previous
+    | '>' -> Token.Next
+    | '+' -> Token.Inc
+    | '-' -> Token.Dec
+    | '.' -> Token.Write
+    | ',' -> Token.Read
+    | '[' -> Token.LoopStart
+    | ']' -> Token.LoopEnd
+
 [<EntryPoint>]
 let main argv =
     
