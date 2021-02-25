2befc3f335b14e817fef3525a3630afc38195822
vstreltsov <vstreltsov@softailor.ru>
2021-02-25 18:18:59 +0700

    Added tokenizing

--- BrainF__k interpreter/Program.fs
+++ BrainF__k interpreter/Program.fs
@@ -12,19 +12,32 @@ type Token =
 
 let maxOperationsCount = int <| 1e5
 
-let readToken = 
-    let symbol = char <| Console.Read()
-    match symbol with
-    | '<' -> Token.Previous
-    | '>' -> Token.Next
-    | '+' -> Token.Inc
-    | '-' -> Token.Dec
-    | '.' -> Token.Write
-    | ',' -> Token.Read
-    | '[' -> Token.LoopStart
-    | ']' -> Token.LoopEnd
-
 [<EntryPoint>]
 let main argv =
-    
+    let linesNumber = (Console.ReadLine() |> (fun s -> s.Split [|' '|]) |> Array.map (int)).[1]
+    let input = Console.ReadLine() |> (fun s -> s.TrimEnd('$')) |> Seq.toList
+
+    let rec getTokens currentLineNumber tokens = 
+        let alphabet = [|'>'; '<'; '+'; '-'; ','; ','; '['; ']'|]
+
+        let getToken = function
+            | '<' -> Token.Previous
+            | '>' -> Token.Next
+            | '+' -> Token.Inc
+            | '-' -> Token.Dec
+            | '.' -> Token.Write
+            | ',' -> Token.Read
+            | '[' -> Token.LoopStart
+            | ']' -> Token.LoopEnd
+
+        match currentLineNumber with
+        | linesNumber -> tokens
+        | _ -> 
+            let newTokens = Console.ReadLine() |> Seq.toList |> List.filter (fun c -> Array.contains c alphabet) |> List.map (fun c -> getToken c)
+            getTokens (currentLineNumber + 1) (tokens @ newTokens)
+
+    let tokens = getTokens 1 []
+    let (memory: byte[]) = Array.zeroCreate maxOperationsCount
+
+
     0
