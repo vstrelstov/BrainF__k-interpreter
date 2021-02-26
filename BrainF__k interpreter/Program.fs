92e71fa4b02e8d44cbd6d9cec64ef676c224996f
Vaas <streltcov.vasiliy.bpo1401@yandex.ru>
2021-02-26 23:00:16 +0700

    Added interpreting function (no loops supported yet)

--- BrainF__k interpreter/Program.fs
+++ BrainF__k interpreter/Program.fs
@@ -37,7 +37,37 @@ let main argv =
             getTokens (currentLineNumber + 1) (tokens @ newTokens)
 
     let tokens = getTokens 1 []
-    let (memory: byte[]) = Array.zeroCreate maxOperationsCount
+    // TODO: Probably use list instead of array. Keep in mind that memory length can be infinite
+    let (memory: byte[]) = Array.zeroCreate maxOperationsCount 
+    
+    let interpret tokens memory = 
+        let tryTail list = 
+            match list with
+            | [] -> []
+            | _::tail -> tail
 
+        // TODO: Consider using of currying or partial application
+        let rec interpreterLoop tokens (memory: byte []) dataPointer input output =
+            match tokens with
+            | [] -> output
+            | head::tail -> 
+                match head with
+                | Token.Next -> interpreterLoop tail memory (dataPointer + 1) input output
+                | Token.Previous -> interpreterLoop tail memory (dataPointer - 1) input output
+                | Token.Inc -> 
+                    memory.[dataPointer] <- (memory.[dataPointer] + 1uy)
+                    interpreterLoop tail memory dataPointer input output
+                | Token.Dec -> 
+                    memory.[dataPointer] <- (memory.[dataPointer] - 1uy)
+                    interpreterLoop tail memory dataPointer input output
+                | Token.Read -> 
+                    memory.[dataPointer] <- (List.head input |> byte)
+                    interpreterLoop tail memory dataPointer (tryTail input) output
+                | Token.Write -> interpreterLoop tail memory dataPointer input (Array.append output [|(memory.[dataPointer] |> char)|])
+
+        interpreterLoop tokens memory 0 input [||]
+
+    let output = interpret tokens memory |> Array.map (fun c -> c.ToString()) |> String.concat ""
+    Console.WriteLine(output)
 
     0
