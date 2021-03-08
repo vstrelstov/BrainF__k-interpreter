469ab136d0b6a9304a92b968d8ff1cfd14a10753
vstreltsov <streltcov.vasiliy.bpo1401@yandex.ru>
2021-03-08 22:58:53 +0700

    Added more line breaks

--- BrainF__k interpreter/Program.fs
+++ BrainF__k interpreter/Program.fs
@@ -42,7 +42,6 @@ let main argv =
             let newCommands = Console.ReadLine() |> Seq.toArray |> Array.filter (fun c -> Array.contains c alphabet) |> Array.map (fun c -> getCommand c)
             getCommands (currentLineNumber + 1) (Array.append commands newCommands)
 
-    // TODO: Consider using list instead of array. Keep in mind that memory length can be infinite 
     let program = { Commands = (getCommands 1 [||]); Memory = Array.zeroCreate maxOperationsCount; }
 
     let execute program =
@@ -52,7 +51,10 @@ let main argv =
                 Console.WriteLine("\nPROCESS TIME OUT. KILLED!!!")    
             elif commandPointer < (Array.length program.Commands) then               
                 let modifyMemoryCell increment = 
-                    program.Memory.[dataPointer] <- if increment then program.Memory.[dataPointer] + 1u else program.Memory.[dataPointer] - 1u
+                    program.Memory.[dataPointer] <- 
+                        if increment then 
+                            program.Memory.[dataPointer] + 1u 
+                        else program.Memory.[dataPointer] - 1u
 
                 let tryTail list = 
                     match list with
@@ -79,8 +81,10 @@ let main argv =
                 | Command.Dec -> 
                     modifyMemoryCell false
                     executionLoop (opCount + 1) (commandPointer + 1) dataPointer input
-                | Command.Previous -> executionLoop (opCount + 1) (commandPointer + 1) (dataPointer - 1) input
-                | Command.Next -> executionLoop (opCount + 1) (commandPointer + 1) (dataPointer + 1) input
+                | Command.Previous -> 
+                    executionLoop (opCount + 1) (commandPointer + 1) (dataPointer - 1) input
+                | Command.Next -> 
+                    executionLoop (opCount + 1) (commandPointer + 1) (dataPointer + 1) input
                 | Command.Read ->
                     program.Memory.[dataPointer] <- (List.tryHead input).Value |> uint
                     executionLoop (opCount + 1) (commandPointer + 1) dataPointer (tryTail input)
@@ -91,12 +95,14 @@ let main argv =
                     if program.Memory.[dataPointer] = 0u then 
                         let newCommandPointer = jumpToLoopBound Command.LoopEnd Command.LoopStart 1
                         executionLoop (opCount + 1) newCommandPointer dataPointer input
-                    else executionLoop (opCount + 1) (commandPointer + 1) dataPointer input
+                    else 
+                        executionLoop (opCount + 1) (commandPointer + 1) dataPointer input
                 | Command.LoopEnd ->
                     if program.Memory.[dataPointer] <> 0u then 
                         let newCommandPointer = jumpToLoopBound Command.LoopStart Command.LoopEnd  -1
                         executionLoop (opCount + 1) newCommandPointer dataPointer input
-                    else executionLoop (opCount + 1) (commandPointer + 1) dataPointer input
+                    else 
+                        executionLoop (opCount + 1) (commandPointer + 1) dataPointer input
 
         executionLoop 0 0 0 input
 
